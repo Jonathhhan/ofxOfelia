26a7df8efe1c4fe8e3d1f477b45a106a97d8d656
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2020-01-26 08:10:27 +0100

    Add files via upload

--- scripts/common/updateMaps.cpp
+++ scripts/common/updateMaps.cpp
@@ -402,10 +402,6 @@ int main(int argc, char *argv[])
     mapper.addFunctionToRename({"pdUnZip", "ofUnZip"});
     mapper.addFunctionToRename({"pdEmscriptenRunScript", "ofEmscriptenRunScript"});
     mapper.addFunctionToRename({"pdEM_ASM", "ofEM_ASM"});
-    mapper.addFunctionToRename({"pdEmbind_1", "ofEmbind_1"});
-    mapper.addFunctionToRename({"pdEmbind_2", "ofEmbind_2"});
-    mapper.addFunctionToRename({"pdEmbind_3", "ofEmbind_3"});
-    mapper.addFunctionToRename({"pdEpochTimeMillis", "ofEpochTimeMillis"});
     /* start writing the file */
     ostringstream ss;
     ss <<
