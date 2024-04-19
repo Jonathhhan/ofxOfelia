7cabc96e5102529440e2e83106667be09ad1bcb3
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-04-19 07:50:55 +0200

    Update updatePdBindings.sh

--- scripts/common/updatePdBindings.sh
+++ scripts/common/updatePdBindings.sh
@@ -4,4 +4,4 @@
 swig -c++ -lua -fcompact -fvirtual -I../../../../libs/openFrameworks -I../../src -o ../../src/ofxOfeliaPdBindings.cpp ofxOfeliaPdBindings.i
 swig -c++ -lua -fcompact -fvirtual -I../../../../libs/openFrameworks -I../../src -o ../../src/ofxOfeliaOfxBindings.cpp ofxOfeliaOfxBindings.i
 swig -c++ -lua -fcompact -fvirtual -I../../../../libs/openFrameworks -I../../src -o ../../src/ofxOfeliaOfxImGuiBindings.cpp ofxOfeliaOfxImGuiBindings.i
-swig -c++ -lua -fcompact -fvirtual -I../../../../libs/openFrameworks -I../../src -o ../../src/ofxOfeliaImGuiPdBindings.cpp ofxOfeliaImGuiBindings.i
+swig -c++ -lua -fcompact -fvirtual -I../../../../libs/openFrameworks -I../../src -o ../../src/ofxOfeliaImGuiBindings.cpp ofxOfeliaImGuiBindings.i
